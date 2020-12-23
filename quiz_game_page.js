dbae2559c49cb9ad421860b0c8afa75fc1923b25
Jaygopal-Lenka <73235238+Jaygopal-Lenka@users.noreply.github.com>
2020-12-23 20:52:29 +0530

    Add files via upload

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -4,6 +4,9 @@ player2_name= localStorage.getItem("player2_name");
 player1_score= 0;
 player2_score= 0;
 
+question_turn ="player1"
+answer_turn= "player2";
+
 document.getElementById("player1_name").innerHTML= player1_name + ":";
 document.getElementById("player2_name").innerHTML= player2_name + ":";
 
@@ -24,5 +27,36 @@ function send() {
     row= question_number + input_box + check_button;
 
     document.getElementById("output").innerHTML=row;
-    document.getElementById("word").value="";
+    document.getElementById("number1").value="";
+    document.getElementById("number2").value="";
+}
+
+function check() {
+  
+    get_answer = document.getElementById("input_check_box").value;
+
+    if (get_answer == actual_answer) {
+         
+        if (answer_turn == "player1") {
+         player1_score= player1_score +1;
+         document.getElementById("player1_score").innerHTML= player1_score; 
+        }
+        else {
+            player2_score= player2_score +1;
+            document.getElementById("player2_score").innerHTML= player2_score; 
+           }
+    }
+    if (question_turn == "player1") {
+        question_turn= "player2";
+        answer_turn= "player1";
+        document.getElementById("player_question").innerHTML= "Question Turn -" + player2_name ;
+        document.getElementById("player_answer").innerHTML= "Answer Turn -" + player1_name; 
+        }
+        else{
+        question_turn= "player1";
+        answer_turn= "player2";
+        document.getElementById("player_question").innerHTML= "Question Turn -" + player1_name ;
+        document.getElementById("player_answer").innerHTML= "Answer Turn -" + player2_name; 
+        }
+        document.getElementById("output").innerHTML= "";
 }
